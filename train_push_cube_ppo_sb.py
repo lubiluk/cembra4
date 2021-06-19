9a274b836f5346bfcf1a1583d9927d576ee22752
Pawel Gajewski <lubiluk@gmail.com>
2021-06-19 16:59:31 +0200

    PPO

---
+++ train_push_cube_ppo_sb.py
@@ -0,0 +1,34 @@
+import gym
+import panda_gym
+
+from stable_baselines.common.policies import MlpLnLstmPolicy
+from stable_baselines.common import make_vec_env
+from stable_baselines import PPO2
+
+from wrappers import DoneOnSuccessWrapper
+from gym.wrappers.flatten_observation import FlattenObservation
+
+SAVE_PATH = "./data/push_sb"
+
+def wrap(env):
+    return FlattenObservation(DoneOnSuccessWrapper(env))
+
+# multiprocess environment
+env = make_vec_env(
+    "PandaPush-v1",
+    n_envs=4,
+    wrapper_class=wrap,
+    env_kwargs={"render": False},
+)
+
+model = PPO2(MlpLnLstmPolicy, env, verbose=1)
+model.learn(total_timesteps=3_000_000)
+model.save(SAVE_PATH)
+
+
+# Enjoy trained agent
+obs = env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
